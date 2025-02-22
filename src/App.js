53bb760bd28267e378e3e8f0bfc02922ceb76374
AlfiyaFullStackDeveloper312 <alfiyapathan970@gmail.com>
2025-02-22 10:15:56 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import "./App.css"; 
+import ImdbDashboard from "./RJ_Movies/ ImdbDashboard";
+ 
+function App(){
+    return(
+      <div className="App">
+ <ImdbDashboard/>
+      </div>
+    )
+}
+export default App;
