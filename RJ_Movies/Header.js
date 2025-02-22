4f66bef8dbe8ab35066f9c4d334ae7ccc7a78784
AlfiyaFullStackDeveloper312 <alfiyapathan970@gmail.com>
2025-02-22 10:11:50 +0530

    Add files via upload

---
+++ RJ_Movies/Header.js
@@ -0,0 +1,22 @@
+ import React from "react";
+import "./Header.css";
+import AddMovie from "./AddMovie";
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div className="Header-Container">
+        <img
+          src="Imdb.webp"
+          style={{ height: "40px", width: "90px" }}
+          alt="Imdb Logo"
+        />
+        <div className="search-container">
+          <input type="text" id="searchInput" placeholder="Search names.." />
+          <p style={{ color: "white" }}></p>
+        </div>
+
+        <AddMovie openForm={this.props.openForm} />
+      </div>
+    );
+  }
+}
